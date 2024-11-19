084a11b2909583d4d322e5c2823defb88ac46e4e
Diogoesteves11 <diogojfe@outlook.com>
2024-11-19 17:46:34 +0000

    Database class done

--- src/Server/server.java
+++ src/Server/server.java
@@ -1,7 +1,13 @@
 package server;
 
+import utils.Database;
+
 public class Server{
+    private final Database database;
+
     public Server(){
-        
+        this.database = new Database();
     }
+
+
 }
--- src/utils/Database.java
+++ src/utils/Database.java
@@ -2,14 +2,24 @@ package utils;
 
 import client.Client;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Database implements Login{
     private final Map<String, Client> clientMap;
+    private int totalClients;
 
     public Database(){
-        clientMap = new HashMap<>();
+        clientMap = new HashMap<String, Client>();
+        this.totalClients = 0;
+    }
+
+    public Database(int totalClients, Map<String, Client> clientMap){
+        this.totalClients = totalClients;
+        this.clientMap = new HashMap<String, Client>(clientMap);
     }
 
     @Override
@@ -24,12 +34,37 @@ public class Database implements Login{
         if(client == null) return false;
         String username = client.getUsername();
 
-        if(!this.clientMap.containsKey(username)){
+        if(!this.clientMap.containsKey(username)) {
             this.clientMap.put(username, client);
+            this.totalClients++;
             return true;
         }
-
         return false;
     }
 
+    public Client getClient(String username){
+        return this.clientMap.get(username);
+    }
+
+    public void serialize(DataOutputStream out) throws IOException {
+        out.writeInt(this.totalClients);
+
+        for (Map.Entry<String, Client> entry : clientMap.entrySet()) {
+            out.writeUTF(entry.getKey());
+            entry.getValue().serialize(out);
+        }
+    }
+
+    public static Database deserialize(DataInputStream in) throws IOException {
+        int totalClients = in.readInt();
+
+        Map<String, Client> tmp_map = new HashMap<>();
+        for (int i = 0; i < totalClients; i++) {
+            String username = in.readUTF();
+            Client client = Client.deserialize(in);
+            tmp_map.put(username, client);
+        }
+
+        return new Database(totalClients, tmp_map);
+    }
 }
