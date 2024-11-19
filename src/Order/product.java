cba912c33c5d5e4e9a07c7c6457f9224c335770c
Diogoesteves11 <diogojfe@outlook.com>
2024-11-19 00:11:23 +0000

    serialization added

--- src/Order/product.java
+++ src/Order/product.java
@@ -1,5 +1,9 @@
 package Order;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
 public class product {
     private static int product_id;
     private final int current_product_id;
@@ -14,6 +18,11 @@ public class product {
     public product(String description) {
         product_id++;
         this.current_product_id = product_id;
+        this.description = description;
+    }
+
+    public product(int id, String description){
+        this.current_product_id = id;
         this.description = description;
     }
 
@@ -40,5 +49,17 @@ public class product {
         if (obj == null || getClass() != obj.getClass()) return false;
         product product = (product) obj;
         return current_product_id == product.current_product_id;
+    }
+
+    public void serialize(DataOutputStream out) throws IOException{
+        out.writeInt(this.getCurrentProductId());
+        out.writeUTF(this.getDescription());
+    }
+
+    public static product deserialize(DataInputStream in) throws IOException{
+        int id = in.readInt();
+        String description = in.readUTF();
+
+        return new product(id,description);
     }
 }
